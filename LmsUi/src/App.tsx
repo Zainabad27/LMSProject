e7487647d88c2d8198a2dafdd19d7f70958d28e9
zainabad <zainabad27@gmail.com>
2026-03-05 16:34:54 +0500

    ..

--- LmsUi/src/App.tsx
+++ LmsUi/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
  return (
     <>
-    <p className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg shadow-md transition-all'>This is zain app bro.</p>
+
     </>
   )
 }
